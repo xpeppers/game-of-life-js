7787b8386e8b365f7715b69783ec0be798425433
giuxtaposition <yg97.cs@gmail.com>
2022-07-12 14:47:56 +0200

    refactored

--- index.mjs
+++ index.mjs
@@ -1,55 +1,52 @@
 export class Grid {
-  constructor (grid) {
-    this.grid = grid
-  }
+    constructor(grid) {
+        this.grid = grid
+    }
 
-  get () {
-    return this.grid
-  }
+    get() {
+        return this.grid.map(row => [...row])
+    }
 
-  getAliveNeighborsFor (x, y) {
-    const isAlive = cell => cell === true
-    return [
-      this.grid[x - 1]?.[y - 1],
-      this.grid[x - 1]?.[y],
-      this.grid[x - 1]?.[y + 1],
-      this.grid[x]?.[y - 1],
-      this.grid[x]?.[y + 1],
-      this.grid[x + 1]?.[y - 1],
-      this.grid[x + 1]?.[y],
-      this.grid[x + 1]?.[y + 1]
-    ].filter(isAlive).length
-  }
+    getAliveNeighborsFor(x, y) {
+        const isAlive = cell => cell === true
+        return [
+            this.grid[x - 1]?.[y - 1],
+            this.grid[x - 1]?.[y],
+            this.grid[x - 1]?.[y + 1],
+            this.grid[x]?.[y - 1],
+            this.grid[x]?.[y + 1],
+            this.grid[x + 1]?.[y - 1],
+            this.grid[x + 1]?.[y],
+            this.grid[x + 1]?.[y + 1],
+        ].filter(isAlive).length
+    }
 
-  evolve () {
+    evolve() {
+        let newGrid = this.get()
 
-    let newGrid = Array(this.grid.length).fill(false).map(()=>Array(this.grid[0].length).fill(false))
+        this.grid.forEach((row, rowIndex) => {
+            row.forEach((cell, columIndex) => {
+                if (this.#shouldDie(rowIndex, columIndex)) {
+                    newGrid[rowIndex][columIndex] = false
+                }
+            })
+        })
 
-    this.grid.forEach((row, rowIndex) => {
-      row.forEach((cell, columIndex) => {
+        this.grid = newGrid
+    }
 
-        if (this.#shouldDie(rowIndex, columIndex)) {
-          newGrid[rowIndex][columIndex] = false
-        } else {
-          newGrid[rowIndex][columIndex] = this.grid[rowIndex][columIndex]
-        }
+    #shouldDie(rowIndex, columIndex) {
+        return (
+            this.#underpopulationAt(rowIndex, columIndex) ||
+            this.#overpopulation(rowIndex, columIndex)
+        )
+    }
 
-      })
-    })
+    #overpopulation(x, y) {
+        return this.grid[(x, y)] && this.getAliveNeighborsFor(x, y) > 3
+    }
 
-    this.grid = newGrid
-  }
-
-  #shouldDie(rowIndex, columIndex) {
-    return this.#underpopulationAt(rowIndex, columIndex) || this.#overpopulation(rowIndex, columIndex);
-  }
-
-  #overpopulation(x, y) {
-    return this.grid[x, y] && this.getAliveNeighborsFor(x, y) > 3;
-  }
-
-  #underpopulationAt (x, y) {
-    return this.grid[x, y] && this.getAliveNeighborsFor(x, y) < 2
-  }
-
+    #underpopulationAt(x, y) {
+        return this.grid[(x, y)] && this.getAliveNeighborsFor(x, y) < 2
+    }
 }
